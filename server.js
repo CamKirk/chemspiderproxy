9ac75641e789a639e47071368e9d060a063051c1
camkirk <camden.kirkland@gmail.com>
2016-10-12 20:39:08 -0500

    it
    changed url

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url= require('url');
 var app = express();
 
 
-app.get('/proxy/:meth/:id', proxy('www.chemspider.com/InChI.asmx/', {
+app.get('/proxy/:meth/:id', proxy('www.chemspider.com', {
     
     filter: function(req,res){
         return req.method=='GET';
@@ -12,13 +12,13 @@ app.get('/proxy/:meth/:id', proxy('www.chemspider.com/InChI.asmx/', {
     forwardPath:function(req,res){
         var meth=req.params.meth;
         var id=req.params.id;
-        console.log(url.parse(req.url+'/InChIKeyToMol?inchi_key='+id).path)
-        return url.parse(req.url+'/InChIKeyToMol?inchi_key='+id).path;
+        console.log(url.parse(req.url+'/InChI.asmx/InChIKeyToMol?inchi_key='+id).path)
+        return url.parse(req.url+'/InChI.asmx/InChIKeyToMol?inchi_key='+id).path;
         
     },
     intercept: function(rsp,data,req,res,callback){
         
-        console.log(rsp);
+        console.log(data);
     }
     
     
