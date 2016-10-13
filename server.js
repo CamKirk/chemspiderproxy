149d9f8eb233f9c452de9badfdde81c929a66762
camkirk <camden.kirkland@gmail.com>
2016-10-12 21:17:13 -0500

    made modular to both i2m and m2i on same route. allow origin write res hed

--- server.js
+++ server.js
@@ -4,47 +4,43 @@ var url= require('url');
 var app = express();
 
 
-app.get('/proxy/i2m/:id', proxy('www.chemspider.com', {
+app.get('/proxy/:mode/:id', proxy('www.chemspider.com', {
+    
+     //deal with annoying as heck favicon requests: https://gist.github.com/kentbrew/763822
+  if(req.url==='/favicon.ico'){
+    res.writeHead(200,{'Content-Type':'image/x-icon'});
+    console.log('favicon req');
+    res.end();
+    return;
+  }
     
     filter: function(req,res){
         return req.method=='GET';
     },
     forwardPath:function(req,res){
-        var meth=req.params.meth;
+        var mode=req.params.mode;
         var id=req.params.id;
-        console.log(res.url);
-        console.log(url.parse('/InChI.asmx/InChIKeyToMol?inchi_key='+id).path)
-        return url.parse('/InChI.asmx/InChIKeyToMol?inchi_key='+id).path;
+       
+        if(mode == i2m){
+            console.log(url.parse('/InChI.asmx/InChIKeyToMol?inchi_key='+id).path)
+            return url.parse('/InChI.asmx/InChIKeyToMol?inchi_key='+id).path;
+        }
+        
+        if(mode == m2i){
+            console.log(url.parse('/InChI.asmx/MoltoInChIKey?inchi_key='+id).path)
+            return url.parse('/InChI.asmx/MoltoInChIKey?inchi_key='+id).path;
+        }
         
     },
     intercept: function(rsp,data,req,res,callback){
         
+        res.writeHead(200,{'Access-Allow-Control-Origin':"*"})
         res.send(data.toString('utf8'));
     }
     
     
 }));
 
-app.get('/proxy/m2i/:id', proxy('www.chemspider.com', {
-    
-    filter: function(req,res){
-        return req.method=='GET';
-    },
-    forwardPath:function(req,res){
-        var meth=req.params.meth;
-        var id=req.params.id;
-        console.log(res.url);
-        console.log(url.parse('/InChI.asmx/MoltoInChIKey?inchi_key='+id).path)
-        return url.parse('/InChI.asmx/MoltoInChIKey?inchi_key='+id).path;
-        
-    },
-    intercept: function(rsp,data,req,res,callback){
-        
-        res.send(data.toString('utf8'));
-    }
-    
-    
-}));
 
 app.listen(process.env.PORT);
 
